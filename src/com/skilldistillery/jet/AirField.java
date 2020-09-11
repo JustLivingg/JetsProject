d7eead1952a15bd8c025c1a9f4b816cc75aa665b
JustLiving <JustinTLivingston@gmail.com>
2020-09-11 16:27:46 -0700

    added fields to airfield

--- src/com/skilldistillery/jet/AirField.java
+++ src/com/skilldistillery/jet/AirField.java
@@ -6,4 +6,13 @@ private Jet[] jets;
 public AirField(Jet[] jets) {
 	this.jets = jets;
 }
+
+public void addJets(Jet jet) {
+	for (int i = 0; i < jets.length; i++) {
+		if (jets[i] == null) {
+			jets[i] = jet;
+			break;
+		}
+	}
+}
 }
--- src/com/skilldistillery/jet/JetsApplication.java
+++ src/com/skilldistillery/jet/JetsApplication.java
@@ -23,7 +23,21 @@ private void launch() {
 }
 
 private void displayUserMenu() {
-	
+System.out.println("*************************************************");	
+System.out.println("* Please select an item from the following menu *");
+System.out.println("*                                               *");
+System.out.println("* 1. List fleet.                                *");
+System.out.println("* 2. Fly all jets.                              *");
+System.out.println("* 3. View fastest jet.                          *");
+System.out.println("* 4. View jet with the longest range.           *");
+System.out.println("* 5. Load all Cargo Jets.                       *");
+System.out.println("* 6. Dogfight!!!!!!                             *");
+System.out.println("* 7. Add a jet to Fleet.                        *");
+System.out.println("* 8. Remove a jet from the Fleet.               *");
+System.out.println("* 9. Quit.                                      *");
+System.out.println("*                                               *");
+System.out.println("*************************************************");
+System.out.println();
 }
 
 
